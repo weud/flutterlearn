1c3109c0bd9ab9f703a1626a8ff3f148612bc7be
Serdar Aysu <Serdar.Aysu@tr.abb.com>
2018-09-27 16:33:41 +0300

    Adding Cards and Immages 2.11

--- aclearn/lib/main.dart
+++ aclearn/lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+    MyApp()); // Fonksiyon Çagırmanın daha kolay bır yolu oldugundan bu sekılde cagırmak daha ıyı; Ayrıca Void bır sey cevırmedıgınden daha az kod satırı demek
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  // OVerride program compıle asamasında programıcıya çağrıldığı classın üzerine yazdığını söylemekte
+  @override
+  //Build Content ve Widget programınd döndüğü tipi söylemekte
+  // normalde gerekli değil ama programa neyin döneceğini söylemek codda yapılacak diğer değişikliklerde
+  // compile alamasında hata olmamasını belirtiyor
+  Widget build(BuildContext contex) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'), 
+              Text('Food Taster')
+              ],
+          ),
         ),
       ),
     );
